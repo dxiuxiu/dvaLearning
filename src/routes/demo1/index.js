8fe177939461292a1311219088b33d7a490298da
duxiuxiu <duxx@summit.com.cn>
2019-12-23 16:36:30 +0800

    chore(.webpackrc): 添加 antd 组件的使用

--- src/routes/demo1/index.js
+++ src/routes/demo1/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'dva';
 
+import {Switch } from 'antd'
 function Demo1() {
+
+  function onChange(checked) {
+    console.log(`switch to ${checked}`);
+  }
   return (
-    <h1> demo1</h1>
+    <h1> demo1
+      <Switch defaultChecked onChange={onChange} />
+    </h1>
   )
 }
 
